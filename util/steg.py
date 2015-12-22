e5d19392fcdab2cb2a9964fffd685bcf412da142
CodeColorist <zhi.zhou@chaitin.com>
2015-12-22 09:13:56 +0800

    steg util

--- util/steg.py
+++ util/steg.py
@@ -1,28 +1,57 @@
 from PIL import Image
 
 import sys
-import os.path
-
-
-base64_lookup = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+import os
+import base64
+import string
 
 
 def steg(payload, image, output):
-    with open(payload, 'rb') as payload_file, \
-            open(image, 'rb') as image_file, \
-            open(output, 'rw') as output_file
-        
-        image_carrier = Image.open(image_file)
-        image_merged = Image.open(output_file)
+    '''
+    Save payload in image and store payload length in "F Number" exif field
 
-        capacity = image_carrier.size
+    one bit per channel
+    16 pixels for one base64 byte
+
+    @payload hidden data
+    @image carrier image
+    @output save destination
+    '''
+
+    base64_lookup = {v:k for k,v in enumerate(
+        string.uppercase + string.lowercase + string.digits + '+/=')}
+
+    with open(payload, 'rb') as file_payload, open(image, 'rb') as file_image:
+        image_carrier = Image.open(file_image)
+        capacity = image_carrier.width * image_carrier.height / 16 * 3 / 4
         payload_size = os.path.getsize(payload)
+
         if payload_size > capacity:
             raise ValueError('Image capacity exceed. Try a nother carrier.')
 
+        if image_carrier.mode != 'RGB':
+            image_carrier = image_carrier.convert('RGB')
 
+        buf = image_carrier.tobytes('raw', 'RGB')
+        encoded = base64.b64encode(file_payload.read())
+        
+    def blend():
+        for offset, ch in enumerate(encoded):
+            val = base64_lookup[ch]
+            for i in xrange(6): # 64 == 2 ^ 6
+                bit = ((val >> i) & 1) ^ 0xFF
+                index = offset * 6 + i
+                yield ord(buf[index]) & bit
 
+    merged = ''.join(map(chr, list(blend())))
+    merged += buf[len(merged):]
 
+    image_merged = Image.frombytes(mode='RGB', size=image_carrier.size, data=merged)
+    image_merged.save(output)
+
+    # add length
+    os.system('exiftool -FNumber=%d %s' % (payload_size, output))
+    
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
