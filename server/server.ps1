46fdf38ee0a4103c9c711ebaad4327cf0de89fe1
周知日 <chichou@ujs.edu.cn>
2015-12-29 22:55:36 +0800

    decode steg info from remote images

--- server/server.ps1
+++ server/server.ps1
@@ -1,12 +1,16 @@
-$tag = "��������"
-$base = "http://www.lofter.com/tag/"
-$images = (Invoke-WebRequest ($base + [System.Web.HttpUtility]::UrlEncode($tag))).Images | 
+Import-Module "steg"
+
+$url = "http://www.lofter.com/tag/%e9%94%9f%e6%96%a4%e6" + 
+    "%8b%b7%e9%94%9f%e6%96%a4%e6%8b%b7%e9%94%9f%e6%96%a4" +
+    "%e6%8b%b7%e9%94%9f%e6%96%a4%e6%8b%b7"
+
+$images = (Invoke-WebRequest ($url)).Images | 
     where {$_.'data-origin'} | % {
         $full = $_.'data-origin' -replace "\?imageView.*$"
         $filename = [System.IO.Path]::GetTempFileName()
         Invoke-WebRequest -OutFile $filename $full
-
-        Write-Host $filename
-        # Remove-Item -Force $filename
+        $buf = Extract-Steg $filename 
+        $info = [System.Text.Encoding]::ASCII.GetString($buf)
+        Write-Host $info
+        Remove-Item -Force $filename
     }
-
